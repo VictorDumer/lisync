3f0dd0381401ac5ec04fde21a0d90a2749401fbd
VictorDumer <victorgabrieldummersouza@gmail.com>
2026-04-16 17:00:04 -0300

    wip: API BACKEND data persistence V1.3

---
+++ src/components/services/fetch.js
@@ -0,0 +1,1 @@
+//TODO: Configurar conexão BFF
--- src/components/services/taskService.js
+++ src/components/services/taskService.js
@@ -1,11 +1,51 @@
 import { createTaskModel } from "../model/taskModel.js";
+import fs from 'fs';
+import path from "path";
+import { fileURLToPath } from "url";
 
 const tasks = []
 let idCounter=1;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const data = path.join(__dirname, '../database/db.json')
+
+
+function checkData(){
+    const check = path.dirname(data)
+    if (!fs.existsSync(check)) fs.mkdirSync(check, { recursive: true });
+    if (!fs.existsSync(data)) fs.writeFileSync(data, '[]', 'utf-8');
+}
+
+function loadData(){
+    try {
+        const dados= fs.readFileSync(data,'utf-8')
+        tasks.length=0;
+        tasks.push(...JSON.parse(dados));
+        idCounter= tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1;
+    } catch (error) {
+        console.error(error)
+        tasks.length=0;
+        idCounter=1;
+    }
+}
+
+function saveData(){
+    try {
+        fs.writeFileSync(data, JSON.stringify(tasks,null,2), 'utf-8');
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+checkData()
+loadData()
+
+
 export const addTask=(title)=>{
     const task = createTaskModel(idCounter++,title)
     tasks.push(task)
+    saveData()
     return task
 }
 
@@ -27,6 +67,7 @@ export const updateTask= (id,title,completed)=>{
     if(completed !== undefined){
         task.completed=completed;
     }
+    saveData()
     return task
 }
 
@@ -35,5 +76,6 @@ export const deleteTask=(id)=>{
     if(index ===-1) return false
 
     tasks.splice(index,1);
+    saveData();
     return true;
 }
